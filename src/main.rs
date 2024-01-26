6b3752a2f06840af04afc58d39ca4e2cb564aa3a
Huseyin Caliskan <huseyin@weave.nl>
2024-01-26 17:21:52 +0100

    feat: zero gravity

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ use systems::*;
 
 use bevy::prelude::*;
 
-use bevy_xpbd_2d::prelude::PhysicsPlugins;
+use bevy_xpbd_2d::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         .add_systems(Startup, spawn_player)
         .add_systems(Startup, spawn_camera)
+        .insert_resource(Gravity(Vec2::NEG_Y * 0.0))
         .run();
 }
